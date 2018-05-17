49140eee6bec93d474788239ac40dca999919146
Damien Marchal <damien.marchal@univ-lille1.fr>
2018-05-18 00:52:54 +0200

    [SPM] ADD a getcomponent script that generates list of components in a plugins.

---
+++ getcomponents.py
@@ -0,0 +1,22 @@
+import Sofa
+
+if len(sys.argv) != 2 and len(sys.argv) != 3:
+        print("USAGE: runSofa getcomponent.py --argv <YourPluginName> <PythonPart>")
+        sys.exit(-1)
+
+def createScene(rootNode):                
+        plugin = sys.argv[1]
+        pyplugin = None
+        if len(sys.argv) == 3:
+                pyplugin = sys.argv[2]
+        rootNode.createObject("RequiredPlugin", name=plugin)
+        
+        components = {}
+        for c in Sofa.getComponentsFromTarget(plugin):
+                if c not in components:
+                        components[c] = c         
+        print('"components" : '+str(components.keys()))
+             
+        if pyplugin != None:                
+                prefabs = {}                
+                print('"components" : '+str(components.keys()))
