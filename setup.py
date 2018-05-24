8daea726786314c0ff4d724954a416b8005b6dcb
Damien Marchal <damien.marchal@univ-lille1.fr>
2018-05-24 14:32:14 +0200

    [SPM] ADD MISING FILES

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup
 
 setup(name='sofa-spm',
-      version='1.0',
+      version='1.0.2',
       description='A package manager for the simulation framework called Sofa (https://www.sofa-framework.org)',
       url='https://github.com/SofaDefrost/SPM',
       author='Damien Marchal',
       author_email='damien.marchal@univ-lille1.fr',
       license='GPL',
-      scripts=['spm/sofa-spm.py'],
+      scripts=['sofa-spm.py'],
+      packages=["spm"],
+      package_data={'spm' : ['spm/recipes/*']},
       install_requires=['mu-repo', 'GitPython'],
+      include_package_data=True,
       zip_safe=True)
---
+++ spm/repo.py
@@ -0,0 +1,3 @@
+import os
+location = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)),"recipes/"))
+
